17f4cbafbbe0965e4d0e5719143d005cc873164d
wolfram444 <109225370+wolfram444@users.noreply.github.com>
2022-08-20 23:33:55 +0300

    Add files via upload

---
+++ js/js/js.js
@@ -0,0 +1,8 @@
+var minute = '60'
+var hourse = '3600'
+var day = '86400'
+
+var nambers = +prompt('Введите секунды') 
+alert(( nambers / minute + ' минут  ') + ( nambers / hourse + ' часов  ') + ( nambers / day + ' дней  '))
+
+console.log(( nambers / minute + ' минут  ') + ( nambers / hourse + ' часов  ') + ( nambers / day + ' дней  ')) ;
